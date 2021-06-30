0b90e15ab77b5a4b38f4eb7afacdda601a23decf
alinamawji <a.mawji08@gmail.com>
2021-06-30 14:33:40 -0500

    completed day 13 exercises

---
+++ module-1/13_Inheritance_Abstract_Classes/exercise-student/java/src/main/java/com/techelevator/RectangleWall.java
@@ -0,0 +1,35 @@
+package com.techelevator;
+
+import org.w3c.dom.css.Rect;
+
+public class RectangleWall extends Wall {
+    private int length;
+    private int height;
+
+    public RectangleWall(String name, String color, int length, int height) {
+        super(name, color);
+        this.length = length;
+        this.height = height;
+    }
+
+    public RectangleWall(String name, String color) {
+        super(name, color);
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getArea() {
+        return length * height;
+    }
+
+    public String toString() {
+        return super.getName() + " (" + length + "x" + height + ") rectangle";
+    }
+}
