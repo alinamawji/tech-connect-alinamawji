d2c5eb562aa38fd0ea13d165fe2cad28212565dc
alinamawji <a.mawji08@gmail.com>
2021-07-12 14:20:20 -0500

    completed module 2 day 1 exercises

--- module-2/01_Introduction_to_Databases/exercise-student/postgres/Exercises/23_parks_opened_in_1960s.sql
+++ module-2/01_Introduction_to_Databases/exercise-student/postgres/Exercises/23_parks_opened_in_1960s.sql
@@ -1,2 +1,4 @@
 -- 23. The name and date established of parks opened in the 1960s (6 rows)
-
+SELECT park_name, date_established FROM park
+WHERE (EXTRACT(YEAR FROM date_established) >= 1960 AND
+       EXTRACT(YEAR FROM date_established) < 1970);
